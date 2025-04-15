21ad60082d227770135b67ea65b265e9025c942f
h668805 <668805@stud.hvl.no>
2025-04-15 10:47:22 +0200

    m

--- Oblig 2/src/main/java/no/hvl/dat109/Forelesning.java
+++ Oblig 2/src/main/java/no/hvl/dat109/Forelesning.java
@@ -1,48 +1,32 @@
 package no.hvl.dat109;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.time.LocalTime;
 import java.util.List;
-import java.util.Map;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
 @Entity
 public class Forelesning {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int forelesningsnr;
-	
 	// en forelesning må ha en lektor?
 	private LocalDate dato;
 	private String tittel;
-	
-	@ManyToOne
-	private Emne emne;
 
-	
-	//private Map<Integer, Integer> tilbakemeldinger;
-	@OneToMany(mappedBy = "forelesning", cascade = CascadeType.ALL)
-
-	private HashMap<Integer, Integer> tilbakemeldinger;
-	
+	@OneToMany(mappedBy = "forelesning")
+	private List<Tilbakemelding> tilbakemeldinger;
 	private double resultat;
 
-	public Forelesning(String tittel, LocalDate dato) {
-		tilbakemeldinger = new HashMap<>();
+	public Forelesning(LocalDate dato, String tittel) {
 		this.dato = dato;
 		this.tittel = tittel;
 		resultat = 0;
 	}
-	
+
 	public LocalDate getDato() {
 		return dato;
 	}
@@ -51,24 +35,28 @@ public class Forelesning {
 		return tittel;
 	}
 
-	public HashMap<Integer, Integer> getTilbakemeldinger() {
-		return tilbakemeldinger;
+	public boolean registrerTilbakemelding(Tilbakemelding tilbakemelding) {
+		int brukernavn = tilbakemelding.getStudent().getBrukernavn();
+		for (Tilbakemelding tm : tilbakemeldinger) {
+			if (brukernavn == tm.getStudent().getBrukernavn()) {
+				return false;
+			}
+		}
+
+		tilbakemeldinger.add(tilbakemelding);
+		return true;
 	}
 
 	public double getResultat() {
 		double sum = 0;
-		for (double i : tilbakemeldinger.values()) {
-			sum += i;
+		for (Tilbakemelding t : tilbakemeldinger) {
+			sum += t.getTilbakemelding();
 		}
 		resultat = sum / (double) tilbakemeldinger.size();
 		return resultat;
 	}
 
 	public boolean giVurdering(int tilbakemelding, Person student) {
-		if (1 <= tilbakemelding && tilbakemelding <= 5 && student != null && !student.erLektor() && !tilbakemeldinger.containsKey(student.getBrukernavn())) {
-			tilbakemeldinger.put(student.getBrukernavn(), tilbakemelding);
-			return true;
-		}
-		return false;
+		return  tilbakemeldinger.add(new Tilbakemelding(tilbakemelding, student));
 	}
 }
